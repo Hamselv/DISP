d6d640e66764b765cefb9433c66f44649c8c6460
Hamselv <88mawa@gmail.com>
2021-03-08 16:22:03 +0100

    Backend

---
+++ backend/backend/Data/HaandvaerkerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using F20ITONK.ASPNETCore.MicroService.ClassLib.Models;
+
+    public class HaandvaerkerContext : DbContext
+    {
+        public HaandvaerkerContext (DbContextOptions<HaandvaerkerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<F20ITONK.ASPNETCore.MicroService.ClassLib.Models.Haandvaerker> Haandvaerker { get; set; }
+
+        public DbSet<F20ITONK.ASPNETCore.MicroService.ClassLib.Models.Vaerktoej> Vaerktoej { get; set; }
+
+        public DbSet<F20ITONK.ASPNETCore.MicroService.ClassLib.Models.Vaerktoejskasse> Vaerktoejskasse { get; set; }
+    }
